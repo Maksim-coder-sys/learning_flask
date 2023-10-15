409301b0bc65964930aef77a180eaa4b8d7a6e6b
Максим Кожарин <mkozharin@gmail.com>
2023-10-15 19:20:04 +0500

    продолжение

--- flsite.py
+++ flsite.py
@@ -5,11 +5,11 @@ menu = ['Установка', 'Первое приложение', 'обратн
 @app.route("/index")
 @app.route("/")
 def index():
-    return render_template("index.html", title = "Про Flask", menu = menu)
+    return render_template("index.html", menu = menu)
 
 @app.route("/about")
 def about():
-    return render_template("about.html", title = "О сайте")
+    return render_template("about.html", title = "О сайте", menu = menu)
 
 if __name__ == "__main__":
     app.run(debug = True)
